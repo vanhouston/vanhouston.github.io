d68c710d643cc54c9f9acf665854c490d699a988
Vien Van <vienhvan@gmail.com>
2017-04-18 17:13:11 -0700

    removed google maps

--- js/script.js
+++ js/script.js
@@ -210,35 +210,35 @@ function isIE() {
 
 
 
-/**
- * @module       RD Google Map
- * @description  Enables RD Google Map Plugin
- */
-;
-(function ($) {
-    var o = $('#google-map');
-
-    if (o.length) {
-        include('//maps.google.com/maps/api/js');
-        $(document).ready(function () {
-            var head = document.getElementsByTagName('head')[0],
-                insertBefore = head.insertBefore;
-
-            head.insertBefore = function (newElement, referenceElement) {
-                if (newElement.href && newElement.href.indexOf('//fonts.googleapis.com/css?family=Roboto') != -1 || newElement.innerHTML.indexOf('gm-style') != -1) {
-                    return;
-                }
-                insertBefore.call(head, newElement, referenceElement);
-            };
-
-            lazyInit(o, function () {
-                o.googleMap({
-                    styles: [{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"color":"#f7f1df"}]},{"featureType":"landscape.natural","elementType":"geometry","stylers":[{"color":"#d0e3b4"}]},{"featureType":"landscape.natural.terrain","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.medical","elementType":"geometry","stylers":[{"color":"#fbd3da"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#bde6ab"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffe15f"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#efd151"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"black"}]},{"featureType":"transit.station.airport","elementType":"geometry.fill","stylers":[{"color":"#cfb2db"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#a2daf2"}]}]
-                });
-            });
-        });
-    }
-})(jQuery);
+// /**
+//  * @module       RD Google Map
+//  * @description  Enables RD Google Map Plugin
+//  */
+// ;
+// (function ($) {
+//     var o = $('#google-map');
+//
+//     if (o.length) {
+//         include('//maps.google.com/maps/api/js');
+//         $(document).ready(function () {
+//             var head = document.getElementsByTagName('head')[0],
+//                 insertBefore = head.insertBefore;
+//
+//             head.insertBefore = function (newElement, referenceElement) {
+//                 if (newElement.href && newElement.href.indexOf('//fonts.googleapis.com/css?family=Roboto') != -1 || newElement.innerHTML.indexOf('gm-style') != -1) {
+//                     return;
+//                 }
+//                 insertBefore.call(head, newElement, referenceElement);
+//             };
+//
+//             lazyInit(o, function () {
+//                 o.googleMap({
+//                     styles: [{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"color":"#f7f1df"}]},{"featureType":"landscape.natural","elementType":"geometry","stylers":[{"color":"#d0e3b4"}]},{"featureType":"landscape.natural.terrain","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.medical","elementType":"geometry","stylers":[{"color":"#fbd3da"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#bde6ab"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffe15f"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#efd151"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"black"}]},{"featureType":"transit.station.airport","elementType":"geometry.fill","stylers":[{"color":"#cfb2db"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#a2daf2"}]}]
+//                 });
+//             });
+//         });
+//     }
+// })(jQuery);
 
 /**
  * @module       RD Navbar
